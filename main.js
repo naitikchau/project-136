ac96cd9194ae935a3f72bd84f6fc3c4e63e6373f
naitikchau <104361541+naitikchau@users.noreply.github.com>
2022-11-09 20:10:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+video = " ";
+Status = " ";
+
+function preload()
+{
+    video = createVideo("");
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,290);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input_text = document.getElementById("input_id").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    Status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
